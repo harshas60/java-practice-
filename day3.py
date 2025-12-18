8fad7e85c28af02f0efa7dde63ee5c492237c462
Harsha vardhan S <harshas1401@gmail.com>
2025-12-18 22:41:04 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,18 @@
+# print("roral coster enterence ")
+# hight = int(input("what is the hight of ur body:"))
+# if (hight>=120):
+#     print("they can ride")
+# else:
+#     print("they cant ride")
+# even_number 12 % 2 == 0
+# number_check=int(input("what is the number u what to check?"))
+# # print((number_check))
+# print(number_check % 2)
+# # number_check = int(input("what is the number you want to check"))
+# if number_check % 2 ==0:
+#     print("its is an even number",number_check)
+# else:
+#     print("it is an odd number:",number_check)    
+
+
+    
