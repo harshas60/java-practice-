aa3a10c6f21e775547f1bad70eaea7fe7a278b27
Harsha vardhan S <harshas1401@gmail.com>
2026-01-05 23:14:56 +0530

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,6 +1,65 @@
 import random
 
-word_list = ["aardvark", " baboon" , "camel" ]
+word_list = ["apple", "chair", "bread", "house", "tiger", "phone", "river", "train",
+    "laptop", "bicycle", "rainbow", "football", "village", "diamond",
+    "monster", "holiday",
+    "astronaut", "algorithm", "microscope", "encyclopedia",
+    "electricity", "architecture", "cryptocurrency",
+    "compiler", "database", "software", "hardware",
+    "variable", "function", "internet", "protocol",
+    "avengers", "batman", "matrix", "inception",
+    "waterfall", "mountain", "hurricane", "volcano" ]
+lives = 6
+stage = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
@@ -26,6 +85,12 @@ while not game_over:
         else:
             display+="_"  
     print(display)   
+    
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            print("you lose")
     if "_" not in display:
         game_over = true
         print("you win")
+    print(stage[lives])
